af6bc42f1e4ae7aed62df9bfe6f4f419c608f9a1
chalindu888 <chalindu888@gmail.com>
2026-08-01 19:19:48 +0530

    ci(devsecops): setup GitHub Actions pipeline with SAST/DAST and automated rollbacks

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
+        destination: `${process.env.API_GATEWAY_URL ?? "http://localhost:8000"}/api/:path*`,
       },
     ];
   },
-=======
-  /* config options here */
->>>>>>> 68a2d6c02c964faad59ad73411bf680c32d82355
 };
 
 export default nextConfig;
